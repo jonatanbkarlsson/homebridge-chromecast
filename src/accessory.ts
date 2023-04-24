357853d6dbc63becd90660e61037b23277583ed1
Jonatan Bergström Karlsson <jonatan.bkarlsson@gmail.com>
2023-04-24 17:03:57 +0200

    setup

--- src/accessory.ts
+++ src/accessory.ts
@@ -9,32 +9,17 @@ export default class Accessory {
     private readonly accessory: PlatformAccessory) {
     this.accessory.getService(this.platform.Service.AccessoryInformation)!
       .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Google Inc.')
-      .setCharacteristic(this.platform.Characteristic.Model, accessory.context.device.txt.md)
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, (accessory.context.device.txt.id).substring(0, 12))
-      .setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.txt.fn)
-      .setCharacteristic(this.platform.Characteristic.ConfiguredName, accessory.context.device.txt.fn)
-      .setCharacteristic(this.platform.Characteristic.ActiveIdentifier, 1)
-      .setCharacteristic(this.platform.Characteristic.SleepDiscoveryMode, this.platform.Characteristic.SleepDiscoveryMode.ALWAYS_DISCOVERABLE);
+      .setCharacteristic(this.platform.Characteristic.Model, this.accessory.context.device.txt.md)
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, (this.accessory.context.device.txt.id).substring(0, 12))
+      .setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.device.txt.fn);
 
-    this.service = this.accessory.getService(this.platform.Service.SmartSpeaker) || this.accessory.addService(this.platform.Service.SmartSpeaker);
+    this.service = this.accessory.getService(this.platform.Service.Switch) || this.accessory.addService(this.platform.Service.Switch);
 
-    // NOTE: Handle active.
-    this.service.getCharacteristic(this.platform.Characteristic.Active)
+    // NOTE: Handle on.
+    this.service.getCharacteristic(this.platform.Characteristic.On)
+      .onGet(() => 1)
       .onSet(value => {
-        this.platform.log.debug('Active', value);
-        this.service.updateCharacteristic(this.platform.Characteristic.Active, 1);
-      })
-
-    // NOTE: Handle input source.
-    this.service.getCharacteristic(this.platform.Characteristic.ActiveIdentifier)
-      .onSet(value => {
-        this.platform.log.debug('Active identifier', value);
-      })
-
-    // NOTE: Handle remote control.
-    this.service.getCharacteristic(this.platform.Characteristic.RemoteKey)
-      .onSet(value => {
-        this.platform.log.debug('Remote key', value);
+        this.platform.log.debug('On', value);
       })
   }
 }
